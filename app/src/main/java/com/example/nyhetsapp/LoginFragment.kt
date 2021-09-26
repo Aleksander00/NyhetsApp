aa985f868a6450767375c0fbd0e0420bad88dc98
Aleksander00 <aleksander00@live.no>
2021-09-26 19:55:27 +0200

    basic UI and navigation, login page + main activity

---
+++ app/src/main/java/com/example/nyhetsapp/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.example.nyhetsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.nyhetsapp.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+    val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
+        R.layout.fragment_login, container, false)
+
+        binding.skipButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+        }
+        return binding.root
+
+        }
+    }
+
---
+++ app/src/main/java/com/example/nyhetsapp/MainFragment.kt
@@ -0,0 +1,25 @@
+package com.example.nyhetsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.nyhetsapp.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater,
+            R.layout.fragment_main, container, false)
+
+        return binding.root
+    }
+
+}
