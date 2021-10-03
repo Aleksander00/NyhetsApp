162bc711208e2d3afe6fca0d582c1c4b141f0aed
aleksander00r <aleksander00@live.no>
2021-10-03 18:59:55 +0200

    Drawermenu for MainFragment

--- app/src/main/java/com/example/nyhetsapp/MainFragment.kt
+++ app/src/main/java/com/example/nyhetsapp/MainFragment.kt
@@ -3,8 +3,11 @@ package com.example.nyhetsapp
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.nyhetsapp.databinding.FragmentMainBinding
 
 
@@ -19,7 +22,8 @@ class MainFragment : Fragment() {
     ): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
 
-        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false) //Removes up button for this fragment
+        //(activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true) //Removes up button for this fragment
+        (activity as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24) //Changes up button icon with drawer icon
         setHasOptionsMenu(true) //Adds option menu
 
         // Inflate the layout for this fragment
@@ -30,6 +34,7 @@ class MainFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.option_menu, menu)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle item selection
         return when (item.itemId) {
@@ -49,11 +54,20 @@ class MainFragment : Fragment() {
                 view?.findNavController()?.navigate(R.id.action_mainFragment_to_startFragment)
                 true
             }
+            android.R.id.home -> { //Overwrites up button
+                if (binding.drawerLayout.isOpen) {
+                    binding.drawerLayout.closeDrawer(GravityCompat.START)
+                } else {
+                    binding.drawerLayout.openDrawer(GravityCompat.START)
+                }
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
-   override fun onDestroyView() {
-       super.onDestroyView()
-       _binding = null
-   }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
