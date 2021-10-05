eafa49be8bf64ca6efdde58874bd5f5a0d06edf6
andreseljasen <andreseljasen@hotmail.com>
2021-10-05 14:21:19 +0200

    darkmode switch (funker ikke)

--- app/src/main/java/com/example/nyhetsapp/ThemeFragment.kt
+++ app/src/main/java/com/example/nyhetsapp/ThemeFragment.kt
@@ -6,16 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+private
 
 class ThemeFragment : Fragment() {
+
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_theme, container, false)
     }
+
+
 
 }
