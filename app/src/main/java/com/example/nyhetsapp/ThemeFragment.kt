80977b3f0a7c5f687183f6f7208df5878055e0d8
Aleksander00 <aleksander00@live.no>
2021-10-06 17:03:35 +0200

    darkmode switch

--- app/src/main/java/com/example/nyhetsapp/ThemeFragment.kt
+++ app/src/main/java/com/example/nyhetsapp/ThemeFragment.kt
@@ -1,27 +1,51 @@
 package com.example.nyhetsapp
 
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import com.example.nyhetsapp.databinding.FragmentThemeBinding
 
-private
 
 class ThemeFragment : Fragment() {
 
+    private var _binding: FragmentThemeBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentThemeBinding.inflate(inflater, container, false)
+        val themeswitch = binding.themeSwitch
 
+        when(context?.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> {themeswitch.toggle()} //Toggles themeswitch if nightmode
+        }
 
-    ): View? {
+        themeswitch.setOnCheckedChangeListener { _, b -> //listener for themeswitch
+            darkmodetoggle(b)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_theme, container, false)
+        return binding.root
     }
 
+    private fun darkmodetoggle(checked: Boolean) { //Toggles nightmode
+        if (checked) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 
 }
