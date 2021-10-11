3b150834bf554367f1a6876e2c89137951fd6a54
Aleksander00 <aleksander00@live.no>
2021-10-11 08:49:47 +0200

    Accountfragment navigation + cleanup

--- app/src/main/java/com/example/nyhetsapp/AccountFragment.kt
+++ app/src/main/java/com/example/nyhetsapp/AccountFragment.kt
@@ -1,22 +1,35 @@
 package com.example.nyhetsapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.nyhetsapp.databinding.FragmentAccountBinding
 
 
 class AccountFragment : Fragment() {
 
-
+    private var _binding: FragmentAccountBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+    ): View {
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
+        binding.backBtn.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_accountFragment_to_mainFragment)
+        }
+        binding.logoutBtn.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_accountFragment_to_startFragment)
+        }
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
